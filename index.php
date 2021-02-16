7cb75038f591ed2b8dc101e79e52fb8b913ee849
salvoab <salvatore.abate85@gmail.com>
2021-02-16 15:41:04 +0100

    Diagramma ER di base per il Blog

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Istruzioni:
+Crea un diagramma per una tabella di db che rappresenti gli Users di un blog.
+Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users.
+Il database e la tabella non devono essere realmente creati.
+
+Bonus (non tanto facoltativo):
+Una volta finita la classe User, pensate a che altre entitá possiamo avere in un sistema di Blogging oltre all'utente.
+Per darvi un idea, un blog ha degli articoli, categorie, tags etc.
+Create nel diagramma anche le altre entitá e definite per ciascuna le rispettive classi con proprietá e metodi.
+*/
+
+?>
